3a134c65e6a76994507eda620d93c3f03aacac36
oop111 <121971831+oop111@users.noreply.github.com>
2024-06-08 05:53:48 +0500

    third commit

--- site/src/test/java/com/community/LoginPageAcceptanceTest.java
+++ site/src/test/java/com/community/LoginPageAcceptanceTest.java
@@ -1,12 +1,16 @@
 package com.community;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,11 +19,24 @@ public class LoginPageAcceptanceTest {
     private WebDriver driver;
 
     @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+    public void setUp() throws IOException {
+        File chromeDriver = new File("/tmp/chromedriver");
+        if (!chromeDriver.exists()) {
+            FileUtils.copyURLToFile(
+                new URL("https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip"),
+                new File("/tmp/chromedriver.zip")
+            );
+            Runtime.getRuntime().exec("unzip /tmp/chromedriver.zip -d /tmp/");
+            new File("/tmp/chromedriver").setExecutable(true);
+        }
+
+        System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver");
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
         driver = new ChromeDriver(options);
     }
 
@@ -27,7 +44,7 @@ public class LoginPageAcceptanceTest {
     public void testLoginPage() {
         driver.get("https://35.157.75.60:8443/login");
         assertEquals("Login - DemoSite", driver.getTitle());
-        }
+    }
 
     @After
     public void tearDown() {
